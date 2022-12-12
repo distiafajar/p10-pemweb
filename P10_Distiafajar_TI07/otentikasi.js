adf401d312c4465c5083328699eb5c2c8d053d92
distiafajar <114464426+distiafajar@users.noreply.github.com>
2022-12-12 07:57:38 -0800

    Add files via upload

---
+++ P10_Distiafajar_TI07/otentikasi.js
@@ -0,0 +1,5 @@
+function kirim(){
+    let inp_nama = document.getElementById("username").value;
+    let inp_pass = document.getElementById("password").value;
+    let form = document.getElementById("form");
+}
